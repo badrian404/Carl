671d6ccf794d43953b6072c86c6632968534506b
badrian404 <30273123+badrian404@users.noreply.github.com>
2026-03-11 09:03:33 -0400

    All files

---
+++ carl_donut_music_data.js
@@ -0,0 +1,8 @@
+window.CARL_DONUT_MUSIC_DATA = {
+  tracks: [
+    { name: "Dungeon Theme", file: "Dungeon Theme.mp3" }
+    // Add more tracks like:
+     ,{ name: "Boss Rush", file: "Boss Rush.mp3" }
+     ,{ name: "Mall of Doom", file: "Mall of Doom.mp3" }
+  ]
+};
